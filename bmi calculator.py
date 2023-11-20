45eb2109e9e0846f1173f53e6c97b769985d9789
GANDIKOTA SUBBARAYUDU <133373105+Gsubbarayudu@users.noreply.github.com>
2023-11-20 18:47:06 +0530

    Add files via upload

---
+++ bmi calculator.py
@@ -0,0 +1,16 @@
+weight = float(input("Enter your weight in kilograms: "))
+height = float(input("Enter your height in meters: "))
+
+bmi = weight / (height ** 2)
+
+if bmi < 18.5:
+    category = "Underweight"
+elif bmi < 25:
+    category = "Normal weight"
+elif bmi < 30:
+    category = "Overweight"
+else:
+    category = "Obese"
+
+print("Your BMI is", round(bmi, 2))
+print("Category:", category)
